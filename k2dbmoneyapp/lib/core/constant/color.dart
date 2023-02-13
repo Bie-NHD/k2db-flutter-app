034d30ce3b113fdf7ec31130b7177f7c59f4e672
K1ETHOANG\kieth <k2db.nckhsv@gmail.com>
2023-02-13 21:38:58 +0700

    set color app

---
+++ k2dbmoneyapp/lib/core/constant/color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorK2DB {
+  static const Color black = Color(0xFF000000);
+  static const Color orange = Color(0xFFFF9900);
+  static const Color grey = Color(0xFFB7B3B2);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color red = Color(0xFFFC0909);
+  static const Color green = Color(0xFF0CC11E);
+  static const Color blue = Color(0xFF0500FF);
+}
