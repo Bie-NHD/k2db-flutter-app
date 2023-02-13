d1fed87ebf3b4061647d2d375d55d40081b30aa3
K1ETHOANG\kieth <k2db.nckhsv@gmail.com>
2023-02-13 21:59:03 +0700

    set dimension for app

---
+++ k2dbmoneyapp/lib/core/constant/dimension.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const double k40Padding = 40;
+const double k30Padding = 30;
+const double k25Padding = 25;
+const double k20Padding = 20;
+const double k10Padding = 10;
+const double k8Padding = 8;
+const double k5Padding = 5;
+
+const double kIconSize = 20;
+
+const double kBorderRadius = 8;
+
+const double kDefault = 15;
