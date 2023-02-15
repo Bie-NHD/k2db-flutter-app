8b2968ad3eb4ed4be2317c123c6aea7996d6a036
huongnttdev <huongntt0123.dev@gmail.com>
2023-02-15 10:27:51 +0700

    change icon app, name app; set routes

--- k2dbmoneyapp/lib/main.dart
+++ k2dbmoneyapp/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:k2dbmoneyapp/core/constant/color.dart';
+import 'package:k2dbmoneyapp/routes.dart';
+import 'package:k2dbmoneyapp/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,32 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: routes,
       title: 'K2DB Money',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'K2DB Money',
-          style: TextStyle(color: ColorK2DB.white),
-        ),
-      ),
+      theme: ThemeData(),
+      home: const HomeScreen(),
     );
   }
 }
---
+++ k2dbmoneyapp/lib/routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:k2dbmoneyapp/screens/home_screen.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  HomeScreen.routeName: (context) => const HomeScreen(),
+};
