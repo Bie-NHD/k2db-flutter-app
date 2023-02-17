7ffdf626ad8eeea63c5b9e497ba25c174c77322f
huongnttdev <huongntt0123.dev@gmail.com>
2023-02-17 18:47:12 +0700

    change color app and padding; margin

--- k2dbmoneyapp/lib/core/constant/color.dart
+++ k2dbmoneyapp/lib/core/constant/color.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
 class ColorsApp {
-  static const Color black = Color(0xFF000000);
-  static const Color orange = Color(0xFFFF9900);
-  static const Color grey = Color(0xFFB7B3B2);
-  static const Color white = Color(0xFFFFFFFF);
-  static const Color red = Color(0xFFFC0909);
-  static const Color green = Color(0xFF0CC11E);
-  static const Color blue = Color(0xFF0500FF);
+  //main color
+  static const Color primaryColor = Color(0xFF29166F);
+  static const Color secondaryColor = Color(0xFFF3D400);
+  static const Color tertiaryColors = Color(0xFFB7B3B2);
+  //background color
+  static const Color backgroundDark = Color(0xFF000000);
+  static const Color backgroundLight = Color(0xFFFFFFFF);
+  //Text color
+  static const Color defaultTextColor = Color(0xFF000000);
+  static const Color headingTextColor = Color(0xFF181717);
+  static const Color titleTextColor = Color(0xFF3B3838);
+  static const Color hintTextColor = Color(0xFF7F7F7F);
+  static const Color appBarTextColor = Color(0xFF7F7F7F);
+  // Status color
+  static const Color statusSuccessColor = Color(0xFF41AF68);
+  static const Color statusErrorColor = Color(0xFFC00000);
+  static const Color statusNoteColor = Color(0xFF2E75B6);
 }
--- k2dbmoneyapp/lib/core/constant/dimension.dart
+++ k2dbmoneyapp/lib/core/constant/dimension.dart
@@ -1,13 +1,17 @@
-const double k40Padding = 40;
-const double k30Padding = 30;
-const double k25Padding = 25;
+const double k24Padding = 24;
 const double k20Padding = 20;
-const double k10Padding = 10;
+const double k16Padding = 16;
+const double k14Padding = 14;
+const double k12Padding = 12;
 const double k8Padding = 8;
-const double k5Padding = 5;
 
-const double kIconSize = 20;
+const double k8Margin = 8;
+const double k12Margin = 12;
 
-const double kBorderRadius = 8;
+const double kIconSize = 22;
+const double kBorderRadiusIcon = 50;
+
+const double kBorderRadiusMin = 12;
+const double kBorderRadiusMax = 24;
 
 const double kDefault = 15;
