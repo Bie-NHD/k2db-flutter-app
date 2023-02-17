4f756941b6fb06686ea590656c856c61e49b7d02
huongnttdev <huongntt0123.dev@gmail.com>
2023-02-17 18:18:07 +0700

    project-structure

--- k2dbmoneyapp/lib/routes.dart
+++ k2dbmoneyapp/lib/routes.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:k2dbmoneyapp/screens/home_screen.dart';
+import 'package:k2dbmoneyapp/views/home_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   HomeScreen.routeName: (context) => const HomeScreen(),
