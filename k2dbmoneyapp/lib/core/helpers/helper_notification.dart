b53aa9a0c5e18d4bdbf6ca971fae2d6b93b5b0d3
MK <k2db.nckhsv@gmail.com>
2023-02-17 20:31:29 +0700

    Setup WebSocket

--- k2dbmoneyapp/lib/core/helpers/helper_notification.dart
+++ k2dbmoneyapp/lib/core/helpers/helper_notification.dart
@@ -1,1 +1,63 @@
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:flutter/material.dart';
 
+class TestWebSocket extends StatefulWidget {
+  const TestWebSocket({Key? key}) : super(key: key);
+
+  @override
+  State<TestWebSocket> createState() => _TestWebSocketState();
+}
+
+class _TestWebSocketState extends State<TestWebSocket> {
+  final TextEditingController _controller = TextEditingController();
+  final _channel = WebSocketChannel.connect(
+    Uri.parse('wss://echo.websocket.events'),
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Test WebSocket"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Form(
+              child: TextFormField(
+                controller: _controller,
+                decoration: const InputDecoration(labelText: 'Send a message'),
+              ),
+            ),
+            const SizedBox(height: 24),
+            StreamBuilder(
+              stream: _channel.stream,
+              builder: (context, snapshot) {
+                return Text(snapshot.hasData ? '${snapshot.data}' : '');
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _sendMessage,
+        tooltip: 'Send message',
+        child: const Icon(Icons.send),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      _channel.sink.add(_controller.text);
+    }
+  }
+
+  @override
+  void dispose() {
+    _channel.sink.close();
+    _controller.dispose();
+    super.dispose();
+  }
+}
