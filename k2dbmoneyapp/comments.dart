01e57adbaeb37a2a268bb80cfbe7317678acfd22
MK <huongntt0123.dev@gmail.com>
2023-02-21 00:15:47 +0700

    ngu ghia

---
+++ k2dbmoneyapp/comments.dart
@@ -0,0 +1,4 @@
+//CÁC BẠN THÊM CÁC WIDDET ĐANG SỬ DỤNG VÀO ĐÂY NHÉ
+
+//IconTextLink: Sử dụng cho See all ----- lib/core/widgets/icon_textlink.dart
+//CardItemFunction: Sử dụng cho dang icon và text các nút chuyển sang tính năng ----- lib/views/components/widgets/widget_card_function.dart
