7f0f640f75a8a5d91291259a761a5bfb5eab08d5
MK <huongntt0123.dev@gmail.com>
2023-02-25 20:41:06 +0700

    Update main page

--- k2dbmoneyapp/lib/views/screens/authen/main_page.dart
+++ k2dbmoneyapp/lib/views/screens/authen/main_page.dart
@@ -4,12 +4,13 @@ import 'package:k2dbmoneyapp/core/constant/color.dart';
 import 'package:k2dbmoneyapp/core/extensions/extension_textstyle.dart';
 import 'package:k2dbmoneyapp/views/screens/store/store_main.dart';
 import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+
 import '../../../../core/constant/dimension.dart';
 import '../../../../core/constant/text.dart';
 import '../home/home_main.dart';
-import '../user/user_main.dart';
 import '../promotion/promotion_main.dart';
 import '../statistic/statistic_main.dart';
+import '../user/user_main.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -25,77 +26,73 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ColorsApp.backgroundLight,
-      bottomNavigationBar: SalomonBottomBar(
-          currentIndex: _currentIndex,
-          onTap: (i) => setState(() => _currentIndex = i),
-          items: [
-            // Home
-            SalomonBottomBarItem(
-              icon: const Icon(
-                FontAwesomeIcons.house,
-                size: kIconSize,
+        backgroundColor: ColorsApp.backgroundLight,
+        bottomNavigationBar: SalomonBottomBar(
+            currentIndex: _currentIndex,
+            onTap: (i) => setState(() => _currentIndex = i),
+            items: [
+              // Home
+              SalomonBottomBarItem(
+                icon: const Icon(
+                  FontAwesomeIcons.house,
+                  size: kIconSize,
+                ),
+                title: Text("Home",
+                    style: TextStyles.defaultStyle.colorDeepBlueText.semiBold),
+                selectedColor: ColorsApp.primaryColor,
+                unselectedColor: ColorsApp.tertiaryColors,
               ),
-              title: Text("Home",
-                  style: TextStyles.defaultStyle.colorBlueText.semiBold),
-              selectedColor: ColorsApp.primaryColor,
-              unselectedColor: ColorsApp.tertiaryColors,
-            ),
-            // Promotion
-            SalomonBottomBarItem(
-              icon: const Icon(
-                FontAwesomeIcons.gift,
-                size: kIconSize,
+              // Promotion
+              SalomonBottomBarItem(
+                icon: const Icon(
+                  FontAwesomeIcons.gift,
+                  size: kIconSize,
+                ),
+                title: Text("Promotion",
+                    style: TextStyles.defaultStyle.colorDeepBlueText.semiBold),
+                selectedColor: ColorsApp.primaryColor,
+                unselectedColor: ColorsApp.tertiaryColors,
               ),
-              title: Text("Promotion",
-                  style: TextStyles.defaultStyle.colorDeepBlueText.semiBold),
-              selectedColor: ColorsApp.primaryColor,
-              unselectedColor: ColorsApp.tertiaryColors,
-            ),
-            // Store
-            SalomonBottomBarItem(
-              icon: const Icon(
-                FontAwesomeIcons.store,
-                size: kIconSize,
+              // Store
+              SalomonBottomBarItem(
+                icon: const Icon(
+                  FontAwesomeIcons.store,
+                  size: kIconSize,
+                ),
+                title: Text("Store",
+                    style: TextStyles.defaultStyle.colorDeepBlueText.semiBold),
+                selectedColor: ColorsApp.primaryColor,
+                unselectedColor: ColorsApp.tertiaryColors,
               ),
-              title: Text("Store",
-                  style: TextStyles.defaultStyle.colorDeepBlueText.semiBold),
-              selectedColor: ColorsApp.primaryColor,
-              unselectedColor: ColorsApp.tertiaryColors,
-            ),
-            // Statistic
-            SalomonBottomBarItem(
-              icon: const Icon(
-                FontAwesomeIcons.chartSimple,
-                size: kIconSize,
+              // Statistic
+              SalomonBottomBarItem(
+                icon: const Icon(
+                  FontAwesomeIcons.chartSimple,
+                  size: kIconSize,
+                ),
+                title: Text("Statistic",
+                    style: TextStyles.defaultStyle.colorDeepBlueText.semiBold),
+                selectedColor: ColorsApp.primaryColor,
+                unselectedColor: ColorsApp.tertiaryColors,
               ),
-              title: Text("Statistic",
-                  style: TextStyles.defaultStyle.colorDeepBlueText.semiBold),
-              selectedColor: ColorsApp.primaryColor,
-              unselectedColor: ColorsApp.tertiaryColors,
-            ),
-            // User
-            SalomonBottomBarItem(
-              icon: const Icon(
-                FontAwesomeIcons.userLarge,
-                size: kIconSize,
+              // User
+              SalomonBottomBarItem(
+                icon: const Icon(
+                  FontAwesomeIcons.userLarge,
+                  size: kIconSize,
+                ),
+                title: Text("User",
+                    style: TextStyles.defaultStyle.colorDeepBlueText.semiBold),
+                selectedColor: ColorsApp.primaryColor,
+                unselectedColor: ColorsApp.tertiaryColors,
               ),
-              title: Text("User",
-                  style: TextStyles.defaultStyle.colorDeepBlueText.semiBold),
-              selectedColor: ColorsApp.primaryColor,
-              unselectedColor: ColorsApp.tertiaryColors,
-            ),
-          ]),
-      body: IndexedStack(
-        index: _currentIndex,
-        children: [
+            ]),
+        body: IndexedStack(index: _currentIndex, children: [
           const HomeScreen(),
           const PromotionScreen(),
-          const  StoreScreen(),
+          const StoreScreen(),
           const Statistic(),
           UserScreen(),
-      ]
-      )
-    );
+        ]));
   }
 }
