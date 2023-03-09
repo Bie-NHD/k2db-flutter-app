ed3be783d44ecd9f2de26a357733b59f774707df
Ng Huong Duyen <huongduyen.work@gmail.com>
2023-03-09 16:39:18 +0700

    update tabbar ̣(2):
    - added ID generator
    - update User
    - update User Tabbar

--- k2dbmoneyapp/lib/views/screens/user/Modal/User.dart
+++ k2dbmoneyapp/lib/views/screens/user/Modal/User.dart
@@ -1,8 +1,14 @@
+import 'package:k2dbmoneyapp/core/extensions/extension_textstyle.dart';
 import 'package:k2dbmoneyapp/core/helpers/helper_asset.dart';
-import 'package:xid/xid.dart';
+import 'package:k2dbmoneyapp/views/screens/user/Widgets/ID_generator.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/constant/color.dart';
+import '../../../../core/constant/dimension.dart';
+import '../../../../core/constant/text.dart';
 
 class User {
-  late final String _userID = Xid().toString();
+  late final String userID = IDGenerator().userID();
   late String userName;
   String? gender;
   int userBalance;
@@ -22,5 +28,54 @@ class User {
       this.userPoint = 0,
       this.userAvatar = HelperAssets.placeholderUserAvatar});
 
-  String get userID => _userID;
+  List<InfoContentItem> infoContent() => [
+        InfoContentItem(title: "Name", content: userName),
+        InfoContentItem(title: "Gender", content: gender ?? "Not specified")
+      ];
+}
+
+List<Widget> securityContent() {
+  return [];
+}
+
+class InfoContentItem extends StatelessWidget {
+  IconData? icon;
+  final String title;
+  final String content;
+  InfoContentItem(
+      {super.key, this.icon, required this.title, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: const BoxDecoration(
+          color: ColorsApp.backgroundLight,
+          boxShadow: [
+            BoxShadow(
+                color: ColorsApp.tertiaryColors,
+                offset: Offset(5, 5),
+                blurRadius: 5)
+          ]),
+      child: Wrap(
+        direction: Axis.horizontal,
+        children: [
+          Icon(
+            icon,
+            size: kIconSize,
+            color: ColorsApp.defaultTextColor,
+          ),
+          Text(
+            title,
+            style: TextStyles.defaultStyle.semiBold.colorDefaultText,
+          ),
+          Text(
+            content,
+            style: TextStyles.defaultStyle.regular.colorDefaultText,
+          )
+        ],
+      ),
+    );
+  }
 }
