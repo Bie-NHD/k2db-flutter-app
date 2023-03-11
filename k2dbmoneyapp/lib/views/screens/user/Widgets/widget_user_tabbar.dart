ee65c30fe757a519ac4191901700ededf706acfc
NguyenHuongDuyen <huongduyen.work@gmail.com>
2023-03-11 15:50:13 +0700

    Update Tabbar (3)
    - Rename and Reformat Code
    - Implement ListView for Info Tab

--- k2dbmoneyapp/lib/views/screens/user/Widgets/widget_user_tabbar.dart
+++ k2dbmoneyapp/lib/views/screens/user/Widgets/widget_user_tabbar.dart
@@ -22,91 +22,107 @@ class UserTabBar extends StatefulWidget {
 }
 
 class _UserTabBarState extends State<UserTabBar> {
-  late User user;
+  late User _user;
   @override
   void initState() {
     super.initState();
-    user = widget._user;
+    _user = widget._user;
   }
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
         length: 2,
-        child: Column(
-          children: [
-            const SizedBox(
-              height: k12Margin,
-            ),
-            TabBar(
-                controller: widget._tabController,
-                labelPadding:
-                    const EdgeInsets.symmetric(horizontal: k12Padding),
-                labelColor: ColorsApp.defaultTextColor,
-                labelStyle: TextStyles.defaultStyle.semiBold.colorDefaultText,
-                indicator: BoxDecoration(
-                    color: ColorsApp.tertiaryColors,
-                    borderRadius: BorderRadius.circular(kBorderRadiusMin)),
-                tabs: [
-                  Tab(
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(FontAwesomeIcons.circleInfo),
-                          SizedBox(
-                            width: k8Padding,
-                          ),
-                          Text("My Account"),
-                        ]),
-                  ),
-                  Tab(
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(FontAwesomeIcons.shield),
-                          SizedBox(
-                            width: k8Padding,
-                          ),
-                          Text("Security"),
-                        ]),
-                  ),
-                ]),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.7,
-              child: TabBarView(
-                controller: widget._tabController,
-                children: [InfoTab(user: user), SecurityTab()],
+        child: Padding(
+          padding: const EdgeInsets.only(top: k12Margin),
+          child: Column(
+            children: [
+              NameTabs(widget: widget),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.7,
+                child: TabBarView(
+                  controller: widget._tabController,
+                  children: [InfoTab(user: _user), const SecurityTab()],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ));
+  }
+}
+
+class NameTabs extends StatelessWidget {
+  const NameTabs({
+    Key? key,
+    required this.widget,
+  }) : super(key: key);
+
+  final UserTabBar widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+        controller: widget._tabController,
+        labelPadding: const EdgeInsets.symmetric(horizontal: k12Padding),
+        labelColor: ColorsApp.defaultTextColor,
+        labelStyle: TextStyles.defaultStyle.semiBold.colorDefaultText,
+        indicator: BoxDecoration(
+            color: ColorsApp.tertiaryColors,
+            borderRadius: BorderRadius.circular(kBorderRadiusMin)),
+        tabs: [
+          Tab(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(FontAwesomeIcons.circleInfo),
+                  SizedBox(
+                    width: k8Padding,
+                  ),
+                  Text("My Account"),
+                ]),
+          ),
+          Tab(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(FontAwesomeIcons.shield),
+                  SizedBox(
+                    width: k8Padding,
+                  ),
+                  Text("Security"),
+                ]),
+          ),
+        ]);
   }
 }
 
 class InfoTab extends StatelessWidget {
   const InfoTab({
     super.key,
-    required this.user,
-  });
+    required User user,
+  }) : _user = user;
 
-  final User user;
+  final User _user;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height,
-      child: Padding(
-        padding: const EdgeInsets.all(k12Padding),
-        child: ConstrainedBox(
-          constraints: BoxConstraints(
-              maxHeight: MediaQuery.of(context).size.height * 0.7),
-          child: ListView.builder(
-              itemCount: user.infoContent().length,
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) => ListTile()),
-        ),
+    return Padding(
+      padding: const EdgeInsets.all(k12Padding),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const EditButton(),
+          Flexible(
+            child: ListView.builder(
+                itemCount: infoContent(user: _user).length,
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) =>
+                    infoContent(user: _user)[index]),
+          ),
+        ],
       ),
     );
   }
@@ -124,7 +140,7 @@ class SecurityTab extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          EditButton(),
+          const EditButton(),
         ],
       ),
     );
@@ -138,10 +154,14 @@ class EditButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: k8Margin),
+      child: Align(
         alignment: Alignment.centerRight,
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
+        child: Wrap(
+            direction: Axis.horizontal,
+            spacing: k8Padding,
+            crossAxisAlignment: WrapCrossAlignment.center,
             children: [
               Text(
                 "Edit",
@@ -155,14 +175,55 @@ class EditButton extends StatelessWidget {
                 FontAwesomeIcons.angleRight,
                 size: kIconSize,
               )
-            ]
-                .map(
-                  (e) => Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: k8Padding / 2),
-                    child: e,
-                  ),
-                )
-                .toList()));
+            ]),
+      ),
+    );
+  }
+}
+
+List<InfoContentItem> infoContent({required User user}) => [
+      InfoContentItem(title: "Name", content: user.userName),
+      InfoContentItem(title: "Gender", content: user.gender ?? "Not specified")
+    ];
+List<Widget> securityContent() => [];
+
+class InfoContentItem extends StatelessWidget {
+  final IconData? icon;
+  final String title;
+  final String content;
+  const InfoContentItem(
+      {super.key, this.icon, required this.title, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: k12Padding),
+      child: ListTile(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(kBorderRadiusMin),
+            side: const BorderSide(color: ColorsApp.tertiaryColors)),
+        leading: Icon(
+          icon,
+          size: kIconSize,
+          color: ColorsApp.defaultTextColor,
+        ),
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: k12Padding),
+          child: Wrap(
+            direction: Axis.horizontal,
+            children: [
+              Text(
+                title,
+                style: TextStyles.defaultStyle.semiBold.colorDefaultText,
+              ),
+              Text(
+                content,
+                style: TextStyles.defaultStyle.regular.colorDefaultText,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
