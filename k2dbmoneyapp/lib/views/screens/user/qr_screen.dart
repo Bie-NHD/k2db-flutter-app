a34432a49e1147b2a2d216db85c1ae8fc67dafc2
NguyenHuongDuyen <huongduyen.work@gmail.com>
2023-04-26 23:38:33 +0700

    [QR Screen] Upate UI

--- k2dbmoneyapp/lib/views/screens/user/qr_screen.dart
+++ k2dbmoneyapp/lib/views/screens/user/qr_screen.dart
@@ -5,6 +5,7 @@ import 'package:k2dbmoneyapp/core/extensions/extension_textstyle.dart';
 import 'package:k2dbmoneyapp/core/helpers/helper_image.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 import '../../../core/constant/color.dart';
 import '../../../core/constant/text.dart';
@@ -25,239 +26,147 @@ class _QRScreenState extends State<QRScreen>
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   late User user = widget.user;
 
-  late TabController _tabController;
   QRViewController? qrViewController;
   int _currentIndex = 0;
   bool _flash = false;
 
-  late List<Widget> screens = [
-    QRView(
-      key: qrKey,
-      onQRViewCreated: _onQRViewCreated,
-    ),
-    const Text("Second tab"),
-  ];
-
   Barcode? result;
+
+  final List<String> appBarName = ["Payment QR", "Scan QR", "Transfer"];
 
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
-    _tabController = TabController(
-      length: 2,
-      vsync: this,
-    );
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    double? height = size.height * 4 / 5;
-    return Container(
-      height: size.height,
-      width: size.width,
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomCenter,
-              colors: [ColorsApp.statusNoteColor, ColorsApp.primaryColor],
-              stops: [0.3, 0.7])),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          shadowColor: Colors.transparent,
-          title: Text(
-            "QR Code",
-            style: TextStyles.defaultStyle.bold.sizeHeading.colorAppBarText,
+
+    return Scaffold(
+      backgroundColor: ColorsApp.backgroundLight,
+      appBar: AppBar(
+        shadowColor: ColorsApp.primaryColor,
+        title: Text(
+          appBarName[_currentIndex],
+          style: TextStyles.defaultStyle.bold.sizeHeading.colorAppBarText,
+        ),
+        centerTitle: true,
+        backgroundColor: ColorsApp.primaryColor,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            FontAwesomeIcons.arrowLeft,
+            color: Colors.white,
           ),
-          centerTitle: true,
-          backgroundColor: ColorsApp.statusNoteColor,
-          leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
+        ),
+      ),
+      bottomNavigationBar: SalomonBottomBar(
+        currentIndex: _currentIndex,
+        onTap: _onTabChange,
+        selectedItemColor: ColorsApp.primaryColor,
+        unselectedItemColor: ColorsApp.secondaryColor,
+        items: [
+          SalomonBottomBarItem(
             icon: const Icon(
-              FontAwesomeIcons.arrowLeft,
-              color: Colors.white,
+              FontAwesomeIcons.qrcode,
+              size: kIconSize,
+            ),
+            title: Text(
+              "Payment QR",
+              style: TextStyles.defaultStyle.bold.colorDeepBlueText,
             ),
           ),
-          actions: [],
-        ),
-        body: Stack(
-          children: [
-            Positioned(
-              top: k12Padding,
-              left: k12Padding,
-              right: k12Padding,
-              child: Container(
-                height: height,
-                // color: Colors.yellow,
-                child: IndexedStack(
-                  index: _currentIndex,
+          SalomonBottomBarItem(
+            icon: const Icon(
+              FontAwesomeIcons.camera,
+              size: kIconSize,
+            ),
+            title: Text(
+              "Scan",
+              style: TextStyles.defaultStyle.bold.colorDeepBlueText,
+            ),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(
+              FontAwesomeIcons.arrowUpFromBracket,
+              size: kIconSize,
+            ),
+            title: Text(
+              "Transfer",
+              style: TextStyles.defaultStyle.bold.colorDeepBlueText,
+            ),
+          ),
+        ],
+      ),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: [
+          PaymentQRView(user: user, height: size.height * 4 / 5),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: k12Margin),
+                child: Text(
+                  "Adjust camera position to scan QR code",
+                  style: TextStyles.defaultStyle.bold.colorDeepBlueText,
+                ),
+              ),
+              Expanded(
+                flex: 5,
+                child: QRView(
+                  key: qrKey,
+                  onQRViewCreated: _onQRViewCreated,
+                  overlayMargin: const EdgeInsets.all(k12Margin),
+                  overlay:
+                      QrScannerOverlayShape(borderRadius: kBorderRadiusMax),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: k12Margin),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Column(
-                      children: [
-                        Text(
-                          "Adjust camera position to scan QR code",
-                          style: TextStyles
-                              .defaultStyle.sizeTitleAndButton.colorAppBarText,
-                        ),
-                        // Text(qrViewController?.getFlashStatus().toString() ?? ""),
-                        const SizedBox(
-                          height: k12Padding,
-                        ),
-                        Expanded(
-                          flex: 5,
-                          child: QRView(
-                            key: qrKey,
-                            onQRViewCreated: _onQRViewCreated,
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: IconButton(
-                            onPressed: _btnCameraFlash,
-                            icon: Icon(_flash
-                                ? FontAwesomeIcons.solidLightbulb
-                                : FontAwesomeIcons.lightbulb),
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          "Scanning for QR code",
-                          style: TextStyles
-                              .defaultStyle.sizeDefault.colorAppBarText,
-                        ),
-                        Text(
-                          result?.code ?? "{result here}",
-                          style: TextStyles
-                              .defaultStyle.sizeDefault.colorAppBarText,
-                        ),
-                      ],
+                    IconButton(
+                      onPressed: () {
+                        //TODO Open Photo Gallery
+                      },
+                      icon: const Icon(FontAwesomeIcons.images),
+                      color: ColorsApp.primaryColor,
                     ),
                     Column(
                       children: [
-                        Center(
-                          child: Container(
-                            // height: height * 0.7,
-                            margin: const EdgeInsets.symmetric(
-                                vertical: k24Padding),
-                            padding: const EdgeInsets.all(k24Padding),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius:
-                                  BorderRadius.circular(kBorderRadiusMin),
-                            ),
-                            child: Column(
-                              children: [
-                                Center(
-                                  child: HelperImage.loadFromAsset(
-                                      user.userAvatar,
-                                      height: height * 0.1),
-                                ),
-                                Text(
-                                  user.userName.toUpperCase(),
-                                  style: TextStyles
-                                      .defaultStyle.sizeHeading.semiBold,
-                                ),
-                                Center(
-                                  child: Container(
-                                    margin: const EdgeInsets.symmetric(
-                                        vertical: k24Padding),
-                                    decoration: BoxDecoration(
-                                        border: Border.all(
-                                            color: ColorsApp.tertiaryColors,
-                                            width: 5),
-                                        borderRadius: BorderRadius.circular(
-                                            kBorderRadiusMax)),
-                                    child: QrImage(
-                                      data: user.security.phoneNum,
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
+                        const Text(
+                          "Scanning for QR code",
                         ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: k24Padding),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              TextButton.icon(
-                                  onPressed: () {
-                                    //TODO Render Image
-                                  },
-                                  icon: const Icon(
-                                    FontAwesomeIcons.download,
-                                    color: Colors.white,
-                                  ),
-                                  label: Text(
-                                    "Download",
-                                    style:
-                                        TextStyles.defaultStyle.colorAppBarText,
-                                  )),
-                              TextButton.icon(
-                                  onPressed: () {
-                                    //TODO System share
-                                  },
-                                  icon: const Icon(
-                                    FontAwesomeIcons.share,
-                                    color: Colors.white,
-                                  ),
-                                  label: Text(
-                                    "Share",
-                                    style:
-                                        TextStyles.defaultStyle.colorAppBarText,
-                                  ))
-                            ],
-                          ),
-                        )
+                        Text(
+                          result?.code ?? "{result here}",
+                        ),
                       ],
+                    ),
+                    IconButton(
+                      onPressed: _btnCameraFlash,
+                      icon: Icon(_flash
+                          ? FontAwesomeIcons.solidLightbulb
+                          : FontAwesomeIcons.lightbulb),
+                      color: ColorsApp.primaryColor,
                     ),
                   ],
                 ),
               ),
-            ),
-            Positioned(
-              left: k12Padding,
-              right: k12Padding,
-              bottom: k12Padding,
-              child: TabBar(
-                controller: _tabController,
-                labelPadding:
-                    const EdgeInsets.symmetric(horizontal: k12Padding),
-                labelColor: Colors.white,
-                unselectedLabelColor: ColorsApp.backgroundLight,
-                labelStyle: TextStyles.defaultStyle.semiBold.colorDefaultText,
-                indicator: BoxDecoration(
-                    color: ColorsApp.statusNoteColor,
-                    borderRadius: BorderRadius.circular(kBorderRadiusMin)),
-                onTap: _onTabChange,
-                tabs: const [
-                  Tab(
-                    text: "Scan",
-                  ),
-                  Tab(
-                    text: "User QR",
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
+            ],
+          ),
+          Container(),
+        ],
       ),
     );
   }
 
   void _onQRViewCreated(QRViewController controller) {
+    // TODO Pause QR View on created
     qrViewController = controller;
     controller.scannedDataStream.listen((scanData) {
       setState(() {
@@ -275,8 +184,7 @@ class _QRScreenState extends State<QRScreen>
   }
 
   void _onTabChange(int index) {
-    _tabController.animateTo(index);
-    if (index == 1) {
+    if (index != 1) {
       qrViewController?.pauseCamera();
     } else {
       qrViewController?.resumeCamera();
@@ -284,5 +192,99 @@ class _QRScreenState extends State<QRScreen>
     setState(() {
       _currentIndex = index;
     });
+  }
+}
+
+class PaymentQRView extends StatelessWidget {
+  const PaymentQRView({
+    super.key,
+    required this.user,
+    required this.height,
+  });
+
+  final User user;
+  final double? height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Center(
+          child: Container(
+            // height: height * 0.7,
+            margin: const EdgeInsets.symmetric(vertical: k24Padding),
+            padding: const EdgeInsets.all(k24Padding),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(kBorderRadiusMin),
+            ),
+            child: Column(
+              children: [
+                Center(
+                  child: HelperImage.loadFromAsset(user.userAvatar,
+                      height: height! * 0.1),
+                ),
+                Text(
+                  user.userName.toUpperCase(),
+                  style: TextStyles.defaultStyle.sizeHeading.semiBold,
+                ),
+                Center(
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: k12Margin),
+                    decoration: BoxDecoration(
+                      border:
+                          Border.all(color: ColorsApp.primaryColor, width: 2),
+                      borderRadius: BorderRadius.circular(kBorderRadiusMax),
+                      boxShadow: [
+                        BoxShadow(
+                          color: ColorsApp.tertiaryColors.withOpacity(.8),
+                          offset: const Offset(5, 10),
+                          blurRadius: 10,
+                        )
+                      ],
+                    ),
+                    child: QrImage(
+                      data: user.security.phoneNum,
+                      padding: const EdgeInsets.all(k24Padding),
+                      backgroundColor: ColorsApp.backgroundLight,
+                      foregroundColor: ColorsApp.primaryColor,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton.icon(
+                onPressed: () {
+                  //TODO Render Image
+                },
+                icon: const Icon(
+                  FontAwesomeIcons.download,
+                  color: ColorsApp.primaryColor,
+                ),
+                label: Text(
+                  "Download",
+                  style: TextStyles.defaultStyle.colorDeepBlueText,
+                )),
+            TextButton.icon(
+                onPressed: () {
+                  //TODO System share
+                },
+                icon: const Icon(
+                  FontAwesomeIcons.share,
+                  color: ColorsApp.primaryColor,
+                ),
+                label: Text(
+                  "Share",
+                  style: TextStyles.defaultStyle.colorDeepBlueText,
+                ))
+          ],
+        )
+      ],
+    );
   }
 }
